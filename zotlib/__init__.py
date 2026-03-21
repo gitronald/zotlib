99185e16b64508763d8608791b2fd8e16145251c
gitronald <gitronald@users.noreply.github.com>
2026-03-21 15:30:25 -0700

    version [prerelease]: 0.4.6a0

--- zotlib/__init__.py
+++ zotlib/__init__.py
@@ -1,6 +1,6 @@
 """Zotlib - Extract and format bibliographic data from Zotero databases."""
 
-__version__ = "0.4.5"
+__version__ = "0.4.6a0"
 
 from zotlib.database import ZoteroDatabase
 from zotlib.extractors import (
